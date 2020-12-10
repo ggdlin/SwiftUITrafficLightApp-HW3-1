9e2950bb01beab758d5cbc87d8ada448c42c0166
Sergey Ivanov <ggdlin@gmail.com>
2020-12-10 16:27:00 +0300

    Initial Commit

---
+++ SwiftUITrafficLightApp-HW3-1/SwiftUITrafficLightApp_HW3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrafficLightApp_HW3_1App.swift
+//  SwiftUITrafficLightApp-HW3-1
+//
+//  Created by Ivanov Sergey on 10.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrafficLightApp_HW3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
